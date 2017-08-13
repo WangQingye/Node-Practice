d7923166ef87aac8d6a64a434fbb98ed14b8647c
WangQingye <329103586@qq.com>
2017-08-13 17:41:37 +0800

    node1

--- testfor.js
+++ testfor.js
@@ -6,7 +6,7 @@ console.log(exec);
 exec("git add .", function(error, stdout, stderr){
 	console.log(stdout);
 });
-exec("git commit -m 'node'", function(error, stdout, stderr){
+exec("git commit -m 'node1'", function(error, stdout, stderr){
 	console.log(stdout);
 });
 exec("git push origin master", function(error, stdout, stderr){
