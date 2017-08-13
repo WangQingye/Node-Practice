9ef9035053999375e3df9794544253cd92358d67
WangQingye <329103586@qq.com>
2017-08-13 17:38:36 +0800

    node

--- testfor.js
+++ testfor.js
@@ -1,9 +1,15 @@
 var fs = require('fs');
 var exec = require('child_process').exec
 var num = Math.ceil(Math.random() * 100)
-fs.writeFileSync('test.txt', num, 'utf8');
+fs.writeFileSync('test1.txt', num, 'utf8');
 console.log(exec);
-exec('git add .', function(error, stdout, stderr){
+exec("git add .", function(error, stdout, stderr){
+	console.log(stdout);
+});
+exec("git commit -m 'node'", function(error, stdout, stderr){
+	console.log(stdout);
+});
+exec("git push origin master", function(error, stdout, stderr){
 	console.log(stdout);
 });
 // exec('git add .');
