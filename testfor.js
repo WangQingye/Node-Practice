bfcf9bc1fa4cbbc4346335fe48ea1763f7fb330b
WangQingye <329103586@qq.com>
2017-08-19 00:10:25 +0800

    99

--- testfor.js
+++ testfor.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var times = process.argv[2];
+console.log(times);
 var exec = require('child_process').exec
 console.log(exec);
 var num = Math.ceil(Math.random() * 100)
@@ -14,6 +16,11 @@ exec("git add .", function(error, stdout, stderr){
 function push(){
 	exec("git push origin master", function(error, stdout, stderr){
 		console.log('push', stdout);
+		if (times >= 1)
+		{
+			console.log('do it again');
+			exec("node testfor " + (times - 1))
+		}
 	})
 }
 setTimeout(push,3000);
