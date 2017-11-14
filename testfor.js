2177695f1cde63786ad6856e05c35026b789f223
wangqingye <329103586@qq.com>
2017-11-14 16:17:33 +0800

    40

--- testfor.js
+++ testfor.js
@@ -3,27 +3,39 @@ var times = process.argv[2];
 console.log(`plan to push ${times} times`);
 var exec = require('child_process').exec
 var num = Math.ceil(Math.random() * 100)
-fs.writeFileSync('test1.txt', num, 'utf8');
 
-exec("git add .", function(error, stdout, stderr){
-	console.log('add', stdout);
-	exec("git commit -m " + num, function(error, stdout, stderr){
-	console.log('commit', stdout);
+//  first pull from repo to avoid the issue if other repo had push 
+exec("git pull .", function(error, stdout, stderr){
+	console.log('pull', stdout);
+	//  second change some file
+	fs.writeFileSync('test1.txt', num, 'utf8');
+	// third add the changes
+	exec("git add .", function(error, stdout, stderr){
+		console.log('add', stdout);
+		// forth commit the changes
+		exec("git commit -m " + num, function(error, stdout, stderr){
+			console.log('commit', stdout);
+			// fifth push the commit
+			exec("git push origin master", function(error, stdout, stderr){
+				console.log('push', stdout);
+				if (error) {
+					console.log('push failed', error)
+					return;
+				}
+				if (times > 1)
+				{
+					console.log('do it again');
+					exec("node testfor " + (times - 1))
+				}
+			})
+		});
 	});
 });
 
-function push(){
-	exec("git push origin master", function(error, stdout, stderr){
-		console.log('push', stdout);
-		if (error) {
-			console.log('push failed', error)
-			return;
-		}
-		if (times > 1)
-		{
-			console.log('do it again');
-			exec("node testfor " + (times - 1))
-		}
-	})
-}
-setTimeout(push,3000);
+
+
+
+// function push(){
+
+// }
+// setTimeout(push,3000);
